fa1a6f6c8f3335a4f03800bccdee01fddde23ee9
vitoman8888 <vito.vittore@gmail.com>
2021-03-27 08:33:34 -0500

    Resume and Portfolio tags

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer'
 
 function App() {
 
-  const [menuSelected, setMenuSelected] = useState('Contact');
+  const [menuSelected, setMenuSelected] = useState('About Me');
 
   return (
     <div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import linkedInLogo from "../../assets/LinkedInLogo.png"
 import gitHubLogo from "../../assets/githubLogoPortfolio.png"
+import targetLogo from "../../assets/TargetStoreLogo.png"
 
 function Footer(props) {
 
     return (
         <footer>
             <div id="footerLinkImages" >
-                <a href="https://www.linkedin.com/in/vito-vittore-ms/">
-                    <img src={linkedInLogo} alt="LinkedInLogo" />
+                <a href="https://www.linkedin.com/in/vito-vittore-ms/" target="_blank" rel="noreferrer">
+                    <img src={linkedInLogo} alt="LinkedIn Logo" />
                 </a> 
-                <a href="https://github.com/vitoman8888">
-                    <img src={gitHubLogo} alt="GithubLogo" />
+                <a href="https://github.com/vitoman8888" target="_blank" rel="noreferrer">
+                    <img src={gitHubLogo} alt="Github Logo" />
+                </a> 
+                <a href="https://www.target.com" target="_blank" rel="noreferrer">
+                    <img src={targetLogo} alt="Target Logo" />
                 </a> 
             </div>
         </footer>
